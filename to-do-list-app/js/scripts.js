de663db9d2d98fd0be093a123df651a3f0866cf2
Chels93 <cacguff@gmail.com>
2024-11-04 15:08:58 -0500

    improved commenting for collaboration in handler.js

---
+++ to-do-list-app/js/scripts.js
@@ -0,0 +1,37 @@
+
+// 1. Adding a new item to the list of items:
+function newItem() {
+    let inputValue = $('#input').val();
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+
+        let crossOutButton = $('<button>X</button>');
+        crossOutButton.on("click", deleteListItem);
+
+        let li = $('<li></li>').text(inputValue);
+        li.append(crossOutButton);
+        $('#list').append(li);
+    }
+
+    // 2. Crossing out an item from the list of items:
+    function deleteListItem() {
+        $(this).toggleClass("strike");
+
+    }
+
+    $(document).on("dblclick", "li", deleteListItem);
+
+    // 3(i). Adding the delete button "X": 
+
+    $('#list').append(crossOutButton);
+
+    // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        $(this).parent().addClass("delete");
+    }
+}
+// 4. Reordering the items: 
+$('#list').sortable();
+
